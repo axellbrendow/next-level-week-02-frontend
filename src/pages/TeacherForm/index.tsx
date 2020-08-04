e4756c3fb2fafc43be24ad72c2a601025aebea2a
axellbrendow <axellbrendow@ioasys.com.br>
2020-08-04 15:13:43 -0300

    Add TeacherForm page (not finished)

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PageHeader from "components/PageHeader";
+
+import * as S from "./styles";
+
+const TeacherForm = () => {
+  return (
+    <S.Container id="page-teacher-form">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </S.Container>
+  );
+};
+
+export default TeacherForm;
---
+++ src/pages/TeacherForm/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: var(--color-text-in-primary);
+  background: var(--color-primary);
+`;
